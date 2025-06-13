e211be720108cb263d35a2c61acd3309c2c4267e
bogyuni <bogyuni@hotmail.com>
2025-06-13 20:16:04 +0900

    react setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import portfolioData from './assets/portfolio.json';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+			<main className='main'>
+				<h1>PortFolio List</h1>
+				{portfolioData.map((item, index) => (
+					<section className='year-section' key={index}>
+						<h2>{item.year}</h2>
+						<ul>
+							{item.projects.map((project, idx) => (
+								<li key={idx}>
+									<a href={project.link} target='_blank' rel="noopener noreferrer">{project.title}</a>
+									<p>{project.description}</p>
+									<p><strong>Technologies:</strong> {project.technologies.join(', ')}</p>
+								</li>
+							))}
+						</ul>
+					</section>
+				))}
+			</main>
     </>
   )
 }
